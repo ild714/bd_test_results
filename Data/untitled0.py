211f9b9018a80ca96e0288b964fbb3f1295538a0
Ильдар <ilas@MacBook-Air-Ilas.local>
2018-08-21 22:04:39 +0300

    My first commit

---
+++ Data/untitled0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 22:59:21 2018
+
+@author: ilas
+"""
+y[0]+=1
+print (y)
---
+++ Data/untitled1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 23:11:33 2018
+
+@author: ilas
+"""
+
+x=['слон']
+y=['слон']
+print (x == y)
